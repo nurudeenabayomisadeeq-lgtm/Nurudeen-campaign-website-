0d94a76f9c0edd266a10517c328fc683a781510c
nurudeenabayomisadeeq-lgtm <nurudeenabayomisadeeq@gmail.com>
2026-08-07 11:57:01 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,23 @@ function showSlides(){
     setTimeout(showSlides,3000);
 }
 <img src="assets/images/nuru1.jpg">
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 3000);
+}
